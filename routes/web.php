45106126e9e87038d80da30a855a4f51716fa3f2
Alvan <alvanmarvianto@gmail.com>
2025-07-18 14:36:38 +0700

    Add technology-based routing and reorganize existing technology route

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,23 @@ Route::get('dashboard', function () {
 
 Route::get('/', [AppController::class, 'index']);
 Route::get('/integration/app/{app_id}', [AppController::class, 'appIntegration'])->name('appIntegration');
-Route::get('/technology/{app_id}', [TechnologyController::class, 'show'])->name('technology');
 Route::get('/integration/stream/{stream}', [AppController::class, 'streamIntegrations'])
     ->name('stream.integrations');
 Route::get('/diagram/stream/{stream}', [AppController::class, 'vueFlowStreamIntegrations'])
     ->name('diagram.stream.integrations');
 
-// Admin routes for stream layout management
+Route::prefix('technology')->group(function () {
+    Route::get('/vendor/{vendor_name}', [TechnologyController::class, 'getAppByVendor'])->name('technology.vendor');
+    Route::get('/os/{os_name}', [TechnologyController::class, 'getAppByOS'])->name('technology.os');
+    Route::get('/database/{database_name}', [TechnologyController::class, 'getAppByDatabase'])->name('technology.database');
+    Route::get('/language/{language_name}', [TechnologyController::class, 'getAppByLanguage'])->name('technology.language');
+    Route::get('/third-party/{third_party_name}', [TechnologyController::class, 'getAppByThirdParty'])->name('technology.third_party');
+    Route::get('/middleware/{middleware_name}', [TechnologyController::class, 'getAppByMiddleware'])->name('technology.middleware');
+    Route::get('/framework/{framework_name}', [TechnologyController::class, 'getAppByFramework'])->name('technology.framework');
+    Route::get('/platfrom/{platform_name}', [TechnologyController::class, 'getAppByPlatform'])->name('technology.platform');
+    Route::get('/{app_id}', [TechnologyController::class, 'show'])->name('technology.app');
+});
+
 Route::prefix('admin')->group(function () {
     Route::get('/stream/{stream}', [AdminVueFlowController::class, 'show'])->name('admin.stream.show');
     Route::post('/stream/{stream}/layout', [AdminVueFlowController::class, 'saveLayout'])->name('admin.stream.layout');
