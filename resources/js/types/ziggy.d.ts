0541ff8022d9d58c6a3e6402fe87309824e012b7
Alvan <alvanmarvianto@gmail.com>
2025-07-21 16:13:26 +0700

    fix 419

--- resources/js/types/ziggy.d.ts
+++ resources/js/types/ziggy.d.ts
@@ -1,12 +1,9 @@
-declare function route(
-    name: string,
-    params?: Record<string, any> | Array<any>,
-    absolute?: boolean,
-    config?: Record<string, any>
-): string;
+interface RouteFunction {
+    (name: string, params?: Record<string, any> | Array<any>, absolute?: boolean, config?: Record<string, any>): string;
+}
 
 declare module 'ziggy-js' {
-    export { route };
+    export const route: RouteFunction;
     export const Ziggy: Record<string, any>;
     export function ZiggyVue(vue: any): void;
 } 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import path from 'path';
 
@@ -11,7 +10,6 @@ export default defineConfig({
             ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
-        tailwindcss(),
         vue({
             template: {
                 transformAssetUrls: {
